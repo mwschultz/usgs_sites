4ba048d0ec4a8e0a8063b06e672e50d0207c1ce1
Matt Schultz <matt@mwschultz.com>
2018-10-24 00:26:10 -0400

    Updated the controller and index to set up for multiple params to the usgs web service

--- app/models/water.rb
+++ app/models/water.rb
@@ -1,14 +1,11 @@
 class Water
   include HTTParty
-  debug_output $stdout
-  #hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || "www.food2fork.com"
-  #base_uri "https://#{hostport}/api"
   base_uri "https://waterservices.usgs.gov/nwis/iv"
   default_params format: "json", siteStatus: "active" #return the data in JSON format, and make sure it is active.
   format :json
 
-  def self.for(state,data_point,site_type)
-    response = get("/", query: {stateCd: state, parameterCd: data_point, siteType: site_type})['value']['timeSeries']
+  def self.for(state,data_point_string,site_type)
+    response = get("/", query: {stateCd: state, parameterCd: data_point_string, siteType: site_type})['value']['timeSeries']
     response
   end
 end

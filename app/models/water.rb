26b95c54d2e70ab12d6aa30bbeaa56d42a2d827b
Matt Schultz <matt@mwschultz.com>
2018-10-24 00:14:59 -0400

    Updated the water.rb to include options for the HTTPget request

--- app/models/water.rb
+++ app/models/water.rb
@@ -1,13 +1,14 @@
 class Water
   include HTTParty
+  debug_output $stdout
   #hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || "www.food2fork.com"
   #base_uri "https://#{hostport}/api"
-  #default_params key: ENV['FOOD2FORK_KEY'] || "5ca60b6295af841d9cd7ccfa00bae109"
+  base_uri "https://waterservices.usgs.gov/nwis/iv"
+  default_params format: "json", siteStatus: "active" #return the data in JSON format, and make sure it is active.
   format :json
 
-  def self.for(state)
-    #get("/search", query: { q: keyword})["recipes"]
-    response = get("https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=#{state}&parameterCd=00010&siteType=ST&siteStatus=active")['value']['timeSeries']
+  def self.for(state,data_point,site_type)
+    response = get("/", query: {stateCd: state, parameterCd: data_point, siteType: site_type})['value']['timeSeries']
     response
   end
 end
